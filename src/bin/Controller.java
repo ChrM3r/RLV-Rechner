2c3aa5d8868c5d5a5faf2ab9f869dfa6f1155822
ChrM3r <chris.merscher@beuth-hochschule.de>
2018-06-24 14:20:13 +0200

    Initial commit

--- src/bin/Controller.java
+++ src/bin/Controller.java
@@ -18,10 +18,12 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.StringTokenizer;
 
 public class Controller {
 
+    private static String ergbnisClipboard;
     @FXML
     private DatePicker postEingangsDatum = new DatePicker();
     @FXML
@@ -35,7 +37,6 @@ public class Controller {
     @FXML
     private Label versRestErgebnis = new Label();
 
-
     private static int restTageSonstige(LocalDate datum1, LocalDate datum2) {
 
         return Days.daysBetween(datum1, datum2).getDays();
@@ -47,7 +48,7 @@ public class Controller {
 
         String[] datumGetrennt = eingabeDatum.split("[.]");
 
-        if (datumGetrennt.length !=3) {
+        if (datumGetrennt.length != 3) {
             datum = new LocalDate(9999, 12, 31);
         } else {
             int tag = Integer.parseInt(datumGetrennt[0]);
@@ -61,24 +62,34 @@ public class Controller {
     private static int restTageJE(LocalDate datum) {
 
         Calendar cal = new GregorianCalendar();
-        LocalDate jahresEnde = new LocalDate(cal.get(Calendar.YEAR),12, 31);
+        LocalDate jahresEnde = new LocalDate(cal.get(Calendar.YEAR), 12, 31);
 
         return Days.daysBetween(datum, jahresEnde).getDays();
     }
 
-    private static String erstattungBerechnen(double versPraemie, int versDauer, int versRestLfz){
+    private static String erstattungBerechnen(double versPraemie, int versDauer, int versRestLfz) {
 
-        double ergebnis = versPraemie - ((versPraemie * versDauer)/versRestLfz);
+        double ergebnis = versPraemie - ((versPraemie * versDauer) / versRestLfz);
 
         DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
         dfs.setDecimalSeparator(',');
         DecimalFormat dFormat = new DecimalFormat("0.00", dfs);
+
         return dFormat.format(ergebnis);
 
     }
+    private static String tausenderZeichen(String eingabe) {
+
+        DecimalFormat df = (DecimalFormat) (DecimalFormat.getInstance(Locale.GERMAN));
+        DecimalFormatSymbols symbols = df.getDecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+        df.setDecimalFormatSymbols(symbols);
+
+        return df.format(Double.parseDouble(eingabe.replace(',', '.')));
+    }
 
     @FXML
-    public void berechnenButton(ActionEvent e){
+    public void berechnenButton(ActionEvent e) {
 
         LocalDate postEingang;
         LocalDate auszahlung;
@@ -91,27 +102,22 @@ public class Controller {
         String token;
         Boolean b = false;
 
-        while(st1.hasMoreTokens()){
+        while (st1.hasMoreTokens()) {
             token = st1.nextToken();
             if (token.matches("[A-Za-z]"))
                 b = true;
         }
 
-        while(st2.hasMoreTokens()){
+        while (st2.hasMoreTokens()) {
             token = st2.nextToken();
             if (token.matches("[A-Za-z]"))
                 b = true;
         }
 
-        if (postEingangsDatum.getEditor().getText().isEmpty() || auszahlungsDatum.getEditor().getText().isEmpty() || versPraemie.getText().isEmpty() || b){
+        if (postEingangsDatum.getEditor().getText().isEmpty() || auszahlungsDatum.getEditor().getText().isEmpty() || versPraemie.getText().isEmpty() || b)
+            eingabeFehlerAlert();
 
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Achtung");
-            alert.setHeaderText("Angaben nicht vollständig oder falsch!");
-            alert.setContentText("Bitte geben Sie ein gültiges Posteingangsdatum, Auszahlungsdatum sowie eine Versicherungsprämie ein.");
-            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-            alert.showAndWait();
-        } else {
+        else {
 
             double versPraemieDouble = Double.parseDouble(versPraemie.getText().replace(',', '.'));
 
@@ -124,7 +130,10 @@ public class Controller {
                 auszahlung = datumAusEingabe(auszahlungsDatum.getEditor().getText());
                 versDauer = restTageSonstige(auszahlung, postEingang);
                 versRestLfz = restTageJE(auszahlung);
-                ergebnis.setText(erstattungBerechnen(versPraemieDouble, versDauer, versRestLfz));
+                ergbnisClipboard = erstattungBerechnen(versPraemieDouble, versDauer, versRestLfz);
+
+                ergebnis.setText(tausenderZeichen(ergbnisClipboard));
+
 
                 if (versDauer == 1)
                     versDauerErgebnis.setText(String.format("Versicherungsdauer: %d Tag", versDauer));
@@ -139,7 +148,7 @@ public class Controller {
         }
     }
 
-    private void datumFehlerAlert(){
+    private static void datumFehlerAlert() {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Fehler");
         alert.setHeaderText("Datumsformat falsch");
@@ -147,28 +156,39 @@ public class Controller {
         alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
         alert.showAndWait();
     }
+
+    private static void eingabeFehlerAlert(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Achtung");
+        alert.setHeaderText("Angaben nicht vollständig oder falsch!");
+        alert.setContentText("Bitte geben Sie ein gültiges Posteingangsdatum, Auszahlungsdatum sowie eine Versicherungsprämie ein.");
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.showAndWait();
+    }
+
     @FXML
-    public void kopierenButton(ActionEvent e){
+    public void kopierenButton(ActionEvent e) {
 
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent content = new ClipboardContent();
-        content.putString(ergebnis.getText());
+
+        content.putString(ergbnisClipboard);
         clipboard.setContent(content);
     }
 
     @FXML
-    public void ueberMenuItem(ActionEvent e){
+    public void ueberMenuItem(ActionEvent e) {
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Über...");
-        alert.setHeaderText("Version: 1.0 Build 20180621");
+        alert.setHeaderText("Version: 1.0 Build 20180622");
         alert.setContentText("Chris Merscher \nLBS Ostdeutsche Landesbausparkasse AG \n71-30 Anwendungsbetreuung und -entwicklung");
         alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
         alert.showAndWait();
     }
 
     @FXML
-    public void exitItem(ActionEvent e){
+    public void exitItem(ActionEvent e) {
         Platform.exit();
     }
 }
